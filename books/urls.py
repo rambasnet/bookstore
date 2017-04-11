a6f9be85d7585f19eb108050b274c99302e16a85
Ram Basnet <rambasnet@gmail.com>
2017-04-10 22:43:58 -0600

    Added all the pages for menu

--- books/urls.py
+++ books/urls.py
@@ -1,7 +1,9 @@
 from django.conf.urls import url
-from .views import index, books
+from .views import index, books, contact, deals
 
 urlpatterns = [
     url(r'^$', index, name='index'),
-    url(r'books/$', books, name='books')
+    url(r'books/$', books, name='books'),
+    url(r'deals/', deals, name='deals'),
+    url(r'contact/$', contact, name='contact')
 ]
--- books/views.py
+++ books/views.py
@@ -19,3 +19,39 @@ def books(request):
                 ('christmas deal', 'https://placehold.it/150x80?text=IMAGE', 'product 1 name', 'descrtiption 1')]
             }
     return render(request, 'books/books.html', context)
+
+def deals(request):
+    context = {
+        'nbar': 'deals',
+        'pageTitle': 'deals',
+        'panelTitle': 'Deals',
+        'panelBody': '<strong>Sorry! There are no deals at this time!</strong>'
+    }
+    return render(request, 'books/static.html', context)
+
+def contact(request):
+    context = {
+        'nbar': 'contact',
+        'pageTitle': 'Contact',
+        'panelTitle': 'Contact',
+        'panelBody': """
+            <!-- List group -->
+            <ul class="list-group">
+            <li class="list-group-item"><strong>Corporate Office: </strong><br />
+                <address>111 University Blvd<br>
+                        Grand Junction, CO 81501 <br>
+                        &phone;: (970) 123-4567<br>
+                        <span class="glyphicon glyphicon-envelope"></span>: help@amazing.com<br>
+            </li>
+            <li class="list-group-item"><strong>Denver Office: </strong><br />
+                <address>123 Amazing Street</br>
+                        Denver, CO 81111 <br>
+                        &phone;: (970) 123-1234<br>
+                        <span class="glyphicon glyphicon-envelope"></span>: denver@amazing.com<br>
+            </li>
+            <li class="list-group-item">Porta ac consectetur ac</li>
+            <li class="list-group-item">Vestibulum at eros</li>
+            </ul>
+        """,
+    }
+    return render(request, 'books/static.html', context)
