b60b4aaeb408dcc46297a7d32c19a33f763e5042
Ram Basnet <rambasnet@gmail.com>
2017-04-18 13:09:15 -0600

    Fixed url patterns to make them more general...

--- books/urls.py
+++ books/urls.py
@@ -5,16 +5,16 @@ from . import views
 
 urlpatterns = [
     url(r'^$', views.index, name='index'),
-    url(r'books/$', views.book_list, name='books'),
+    url(r'books/', views.book_list, name='books'),
     url(r'deals/', views.deals, name='deals'),
-    url(r'contact/$', views.contact, name='contact'),
+    url(r'contact/', views.contact, name='contact'),
     url(r'^(?P<id>\d+)/(?P<slug>[-\w]+)/$',
         views.book_detail, 
         name='book_detail'),
-    url(r'subscribe/$', views.subscribe, name='subscribe'),
-    url(r'login/$', views.login, name='login'),
-    url(r'logout/$', views.logout, name='logout'),
-    url(r'signup/$', views.signup, name='signup'),
-    url(r'dashboard/$', views.dashboard, name='dashboard'),
-    url(r'account/$', views.account, name='account')
+    url(r'subscribe/', views.subscribe, name='subscribe'),
+    url(r'^login', views.login, name='login'),
+    url(r'logout/', views.logout, name='logout'),
+    url(r'signup/', views.signup, name='signup'),
+    url(r'dashboard/', views.dashboard, name='dashboard'),
+    url(r'account/', views.account, name='account')
 ]
